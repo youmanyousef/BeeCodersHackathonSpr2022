4aaf187e8d64c8b4cda2ec4de4a16645a4f62775
userisanapple <coolapple14@gmail.com>
2022-03-26 13:37:57 -0700

    reformat models

--- app/admin.py
+++ app/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import StudentUser, Project
+from .models import StudentUser, Project, Comment
 
 # Register your models here.
 admin.site.register(StudentUser)
 admin.site.register(Project)
+admin.site.register(Comment)
