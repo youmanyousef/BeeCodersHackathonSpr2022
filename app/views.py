ab190efbb4d4679430741184485a73986c7a4176
userisanapple <coolapple14@gmail.com>
2022-03-26 15:14:56 -0700

    implement login and logout views, working on project creation page

--- app/views.py
+++ app/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+
+from .models import Project
 
 # Create your views here.
 def index(request):
@@ -10,5 +15,43 @@ def index(request):
     }
     return render(request, 'index.html', context=context)
 
-def projects(request):
-    return HttpResponse()
+def create(request):
+    if request.method == 'GET':
+        return render(request, 'create.html')
+    if request.method == 'POST':
+        title = request.POST.get('project_title')
+        description = request.POST.get('project_desc')
+        print(description)
+        new_project = Project(title=title, description=description, author=request.user, date_time=datetime.now())
+        new_project.save()
+
+def delete(request, project_id):
+    if request.method == 'POST':
+        # get project from db with corresponding id
+        project_obj = Project.objects.get(id=project_id)
+        # delete only if the request user is the author of project
+        if request.user == project_obj.author:
+            project_obj.delete()
+        
+        #todo implement else
+    if request.method == 'GET':
+        # todo implement deletion page, offer projects user has created
+        return render(request, 'index.html') #change template
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse('index'))
+        else:
+            return HttpResponseRedirect(reverse('index'))
+    if request.method == 'GET':
+        return render(request, 'login.html')
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('index'))
