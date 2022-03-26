6f26cfb53a2d9aeafcf34bfeb302ed155b28fc35
userisanapple <coolapple14@gmail.com>
2022-03-26 12:59:46 -0700

    work on implementing models and clean up files

--- app/models.py
+++ app/models.py
@@ -2,19 +2,28 @@ from django.db import models
 
 # Create your models here.
 # student/user data tables
-#class User(models.Model):
+class User(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    school = models.CharField(max_length=60) # Create separate table with colleges and serve recommendations based on existing colleges in db?
+    biography = models.CharField(max_length=200)
+    projects = models.ManyToManyField("Project")
     # Unique primary ID (key)
-    # Name field
-    # College/School Field
+    # Name field                        -- done
+    # College/School Field              -- done 
     # Classes/Equivalent Field
-    # Bio
+    # Bio                               -- done
     # Interests (misc, not needed) 
-    # Projects Followed
-    # Projects Started
+    # Projects Followed                 -- done 
+    # Projects Started                  -- done, projects started will be tracked through author field in project model
     # Friends (misc, not needed)
 
 # Project Listing data tables
-#class Projects(models.Model):
+class Project(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    complete = models.BooleanField(default=False)
+    title = models.CharField(max_length=80)
+    description = models.CharField(max_length=200, blank=True)
     # Unique primary ID (key)
     # Post field (possibly a json object with
         ## Project Name
--- studentfinder/settings.py
+++ studentfinder/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -54,9 +55,7 @@ ROOT_URLCONF = 'studentfinder.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [
-            'studentfinder/www/'
-        ],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
